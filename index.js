67f5f2c49fa44006bc3751cae1915a4753f51aa8
Dario Valenzuela C <dvalenzuela4697@e-camp.cl>
2021-09-06 18:42:59 -0300

    proyeecto SWW funcionando.  Falta comentar y ver opciones para que desde el servidor (node) se pueda acceder al DOM para borrar los campos.  También hay que poner una ruta para que el servidor abra con la página index.html

--- index.js
+++ index.js
@@ -12,6 +12,57 @@ const fs = require("fs");
 
 // Se crea el servidor
 http.createServer( (req, res) => {  //función anónima que tiene un request y un response
+    const params = url.parse(req.url, true).query;
+    console.log(params);
+    const archivo = params.archivo;
+    const contenido = params.contenido;
+    const nombre = params.nombre;
+    const nuevoNombre = params.nuevoNombre;
 
+    if (req.url.includes("/crear")) {
+        fs.writeFile(archivo, contenido, "utf8", () => {
+            console.log(`Archivo ${archivo} creado sin problemas`);
+        })
+    }
+
+    if (req.url.includes("/leer")) {
+        fs.readFile(archivo, "utf8", (err, data) => {
+            if (err){
+                console.log(`Archivo ${archivo} no leído`);
+                throw err;
+            };
+            console.log(`Archivo ${archivo} leído sin problemas:`);
+            console.log(data);
+        })
+    }
+
+    if (req.url.includes("/renombrar")) {
+        fs.rename(nombre, nuevoNombre, (err) => {
+            if (err){
+                console.log("Nada, no se pudo renombrar");
+                throw err;
+            }
+            console.log("Ya se renombró");
+        })
+    }
+
+    if (req.url.includes("/eliminar")) {
+        fs.unlink(archivo, (err) => {
+            if (err){
+                console.log("No se borró, no entiendo porqué");
+                throw err;
+            };
+            console.log("Qué crees, ya se borró");
+        })
+    }
 })
-.listen(PORT, () => console.log('Escuchando el puerto ' + PORT))
+.listen(PORT, () => console.log('Escuchando el puerto ' + PORT));
+
+
+
+
+function limpiarCamposEnHtml () {
+    document.getElementById("archivo").innerHTML = "";
+    document.getElementById("contenido").innerHTML = "";
+    console.log("Supuestamente limpió todo");
+}
